f1e7d47c81a45a1794c87ba905086a786ce0da52
Tim Burris <tim.burris@chalklogic.com>
2024-01-04 08:21:27 -0500

    a demonstration of how you can hook into the OnTokenValidated to lookup your users in a dabase and add their id as a claim

--- src/DemoAzureActiveDirectory/Program.cs
+++ src/DemoAzureActiveDirectory/Program.cs
@@ -3,12 +3,67 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var azureAdSection = builder.Configuration.GetSection("AzureAd");
 
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-  .AddMicrosoftIdentityWebApp(builder.Configuration);
+  .AddMicrosoftIdentityWebApp(configureMicrosoftIdentityOptions: options =>
+  {
+      //setup the required options tying to your Azure AD
+      options.Instance = azureAdSection.GetValue<string>("Instance");
+      options.Domain = azureAdSection.GetValue<string>("Domain");
+      options.TenantId = azureAdSection.GetValue<string>("TenantId");
+      options.ClientId = azureAdSection.GetValue<string>("ClientId");
+      options.CallbackPath = azureAdSection.GetValue<string>("CallbackPath");
+
+      //here setup a callback that will be fired when a user is validated, you can use this to find out who logged in and inspect/add claims
+      options.Events.OnTokenValidated = ctx =>
+      {
+          var identity = ctx.Principal?.Identity as ClaimsIdentity;
+          if (identity == null)
+          {
+              throw new ApplicationException("Unsupported identity");
+          }
+
+          //get the email address from Claims (Azure AD put this fella in here for us, but it's not always obvious where the email address is, because users could omit certain things)
+
+          //try the email address claim
+          var emailAddress = identity.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+          //if we didn't find that, try the "Mail" claim
+          if (emailAddress == null)
+          {
+              emailAddress = identity.Claims.FirstOrDefault(x => x.Type == "mail")?.Value;
+          }
+
+          //if we still don't have it, try their username
+          if (emailAddress == null)
+          {
+              emailAddress = identity.Claims.FirstOrDefault(x => x.Type == "preferred_username")?.Value;
+          }
+
+          if (emailAddress == null)
+          {
+              //are there other claims you might want to try?
+              return Task.CompletedTask;
+          }
+
+          //by this point you should definitely have an address
+
+          //look up ID in your database using the email address
+          Guid customUserId = Guid.NewGuid();
+
+          //add the userid, and any other pertinent info into claims
+          identity.AddClaim(new Claim(type: "CustomUserId", customUserId.ToString()));
+
+          return Task.CompletedTask;
+      };
+
+  }, openIdConnectScheme: OpenIdConnectDefaults.AuthenticationScheme
+            );
 
 builder.Services.AddControllersWithViews(options =>
 {
